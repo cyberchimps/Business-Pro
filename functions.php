a4b0f0f6f704af3e31ff521dffb382ff1c460f2b
Tyler Cunningham <tyler@cyberchimps.com>
2011-11-14 11:45:45 -0800

    should do it for 2.1.1

--- functions.php
+++ functions.php
@@ -195,8 +195,6 @@ if ( function_exists( 'add_theme_support' ) ) {
 	
 // Load jQuery
 	if ( !is_admin() ) {
-	   wp_deregister_script('jquery');
-	   wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"), false);
 	   wp_enqueue_script('jquery');
 	}
 /**
