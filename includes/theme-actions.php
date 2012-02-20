6106aa1956860b6bb056de75f6187d43fa91058f
Tyler Cunningham <tyler@cyberchimps.com>
2012-02-20 11:46:14 -0800

    mobile menu

--- includes/theme-actions.php
+++ includes/theme-actions.php
@@ -336,6 +336,7 @@ function business_logo_menu_content() {
 			<div class="seven columns">
 			<div id="halfnav">
 			<?php wp_nav_menu( array(
+			'items_wrap'      => '<ul id="nav_menu">%3$s</ul>',
 		    'theme_location' => 'sub-menu' // Setting up the location for the main-menu, Main Navigation.
 			    )
 			);
