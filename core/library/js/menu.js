a2e9929ef86f4a404f4a44bedf3868fed17d57be
Tyler Cunningham <tyler@cyberchimps.com>
2012-02-21 23:23:43 -0800

    portfolio bg

--- core/library/js/menu.js
+++ core/library/js/menu.js
@@ -3,6 +3,8 @@ jQuery(document).ready(function($){
 });
 
 jQuery(document).ready(function($){
+
+
 	
 	$("#portfolio_wrap img").hover(function(){
 		$(this).fadeTo("fast", 0.2); 
@@ -10,10 +12,4 @@ jQuery(document).ready(function($){
    		$(this).fadeTo("fast", 1.0); 
 	});
 	
-	$("#portfolio_wrap .portfolio_caption").hover(function(){
-		$(this).fadeTo("fast", 1.0); 
-	},function(){
-   		$(this).fadeTo("fast", 0); 
-	});
-
 });
