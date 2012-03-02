bdb0fb7104ba8e7948e142603cc0ef07c8282336
Tyler Cunningham <tyler@cyberchimps.com>
2012-03-01 17:09:54 -0800

    functioning front-page, still need to determine defaults and order

--- core/pro/actions/slider-actions.php
+++ core/pro/actions/slider-actions.php
@@ -55,7 +55,7 @@ function synapse_slider_content() {
 		$timer = get_post_meta($post->ID, 'slider_timer' , true);	
 	}
 	
-	elseif (is_front_page()) {
+	if (is_front_page()) {
 		$category = $options->get($themeslug.'_front_slider_category'); 
 		$customcategory = $options->get($themeslug.'_front_customslider_category');
 		$captionstyle = $options->get($themeslug.'_front_caption_style');
