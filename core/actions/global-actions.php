64cbc03b29174b276baf7252c1809043a66afecc
Tyler Cunningham <tyler@cyberchimps.com>
2012-04-03 16:54:59 -0700

    should be it for 3.0.5

--- core/actions/global-actions.php
+++ core/actions/global-actions.php
@@ -82,7 +82,7 @@ function business_loop_content($content) {
 						the_excerpt();
 						}
 						else {
-							the_content();
+							the_content(__('Read more...', 'business'));
 						}
 					 ?>
 				<!--Begin @business link pages hook-->
