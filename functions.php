973788ed15d71f0de6f79f05aadb274321f94855
mfssurya <suryap@mindfiresolutions.com>
2012-06-22 19:42:59 +0530

    - Fixed the validation issue with read more link.

--- functions.php
+++ functions.php
@@ -137,7 +137,7 @@ function new_excerpt_more($more) {
     		$linktext = $options->get($themeslug.'_excerpt_link_text');
    		}
 
-	return '<div class="more-link"><a href="'. get_permalink($post->ID) . '"> <br /><br /> '.$linktext.'</a></div>';
+	return '</p><div class="more-link"><a href="'. get_permalink($post->ID) . '">'.$linktext.'</a></div>';
 }
 add_filter('excerpt_more', 'new_excerpt_more');
 
